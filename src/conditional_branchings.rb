1401a292236220199bfcbd44c6f900727d04b638
kshibayama <shibayama.kentaro@gmail.com>
2023-11-22 03:34:12 +0100

    if statements

--- src/conditional_branchings.rb
+++ src/conditional_branchings.rb
@@ -2,3 +2,18 @@
 if true then
 	puts("hoge")
 end
+
+
+hoge = 'fuga'
+
+if hoge == nil then
+	puts('hoge is nil')
+end
+
+hoge = 1 && 0
+
+if hoge then
+	puts(true)
+elsif fuga then
+	puts(false)
+end
