cbf1fbe4266b02da96f1bd13398db39842ed0512
kshibayama <shibayama.kentaro@gmail.com>
2023-11-22 03:12:26 +0100

    references

--- src/variables.rb
+++ src/variables.rb
@@ -10,3 +10,23 @@ puts(CONST)
 
 CONST = "Value changed."
 puts(CONST)
+
+
+# Two different objects
+v1 = "France"
+v2 = "France"
+
+puts(v1.object_id)
+puts(v2.object_id)
+
+# One object in two variables
+v1 = v2
+
+puts(v1.object_id)
+puts(v2.object_id)
+
+# Two different objects
+v1 = "France"
+
+puts(v1.object_id)
+puts(v2.object_id)
