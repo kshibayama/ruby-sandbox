86ca3ed8e045a75db2a70e210ee4e0b1b8ee15e4
kshibayama <shibayama.kentaro@gmail.com>
2023-11-24 01:13:23 +0100

    hash manipulations

--- src/hash.rb
+++ src/hash.rb
@@ -30,3 +30,33 @@ h3 = Hash.new("none")
 puts('default: '+h3.default)
 h3.default = "default"
 puts('default: '+h3.default)
+
+# Hash manupilation
+h4 = { 4 => 40, 5 => 50 }
+h4[6] = 60
+puts(h4)
+h4[6] = 66
+puts(h4[6])
+
+# Hash manipulation with the store method
+h4.store(7, 70)
+puts(h4)
+h4.store(7, 77)
+puts(h4)
+
+# Hash Size
+puts(h4.length)
+
+# Repeating Operation with Hash
+h4.each{|key, value|
+	puts("key: "+key.to_s+"\nvalue: "+value.to_s)
+}
+h4.each_key{|key|
+	puts("key: "+key.to_s)
+}
+h4.each_value{|value|
+	puts("value: "+value.to_s)
+}
+
+h4_array = h4.to_a
+puts(h4_array)
